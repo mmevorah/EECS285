1c0b33eab01792fb06d4b88a54d19f563f2449b0
mmevorah <markmevorah@67-194-80-201.wireless.umnet.umich.edu>
2013-11-25 20:32:45 -0500

    Network Ready

--- Game/src/eecs285/Main.java
+++ Game/src/eecs285/Main.java
@@ -1,9 +1,54 @@
 package eecs285;
 
+import java.awt.BorderLayout;
+import java.net.InetAddress;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
 public class Main {
 
 	public static void main(String[] args){
+		JFrame ipFrame = new JFrame("Get IP Address");
+		JTextField ip_send_field;
+		JButton sendButton;
 		
+		ipFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ipFrame.setSize(400,500);
+		ipFrame.setLayout(new BorderLayout());
+
+		InetAddress ip = null;
+		try{
+			ip = InetAddress.getLocalHost();
+			System.out.println("currentIP:"+ip);
+		}catch(Exception e){
+			
+		}
+		
+		String ipAddress;
+		ipAddress = ip.toString();
+		
+		ip_send_field = new JTextField(20);
+		JLabel prompt = new JLabel (" Please copy the IP Address Seen Here : " + ipAddress);
+		
+	
+		
+		sendButton = new JButton ("Send");
+		
+		
+		ipFrame.add(prompt, BorderLayout.NORTH);
+		ipFrame.add(ip_send_field, BorderLayout.CENTER);
+		ipFrame.add(sendButton, BorderLayout.SOUTH);
+		
+<<<<<<< HEAD
+		String ip = "";
+		Game game = new Game(ip);
+=======
+		ipFrame.setVisible(true);
+>>>>>>> 9cf414924186f7de5a9fb6cb0b72c3f7743b405f
 	}
 	
 }
