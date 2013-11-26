4eed1c8c87fcfd39945e195b7ce4cd0f4296820a
mmevorah <markmevorah@67-194-80-201.wireless.umnet.umich.edu>
2013-11-25 22:31:26 -0500

    Hashed out gamewindow

--- Game/src/eecs285/GameWindow.java
+++ Game/src/eecs285/GameWindow.java
@@ -11,19 +11,34 @@ import javax.swing.JFrame;
  */
 public class GameWindow extends JFrame {
 
+	//Player localPlayer
+	//Player opponentPlayer
+	
 	public GameWindow() {
-		// super("Game");
-		setTitle("Game");
 		setSize(200, 200);
 
+		
+		//Format game window
+		///Size is a medium to large window
+		///Set title to game title (pending)
+		///Set background to an image
+		
 		setVisible(true);
 	}
 
 	public void updateView() {
 
-		// Update Character Positions
-		// Update status stuff ie. health
-
+		//look at player class to get exact names of methods
+		
+		//localPlayer.getImage();
+		//self.displayImageAt( localPlayer.getXCoord() , localPlayer.getYCoord() )
+		//same thing above for opponent player
+		
+		//get health localPlayer.getHealth()
+		//update localPlayerHealthBar
+		//same thing for opponent player
+		
+		
 	}
 
 }
