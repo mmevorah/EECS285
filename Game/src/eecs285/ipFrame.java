8fb2f3ff13ff6ed5c8033da9afa4fa9c615ebbe7
Connor Toohey <cmtoohey@umich.edu>
2013-11-30 18:29:19 -0500

    The ipFrame

--- Game/src/eecs285/ipFrame.java
+++ Game/src/eecs285/ipFrame.java
@@ -11,51 +11,71 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
+@SuppressWarnings("serial")
 public class ipFrame extends JDialog {
 
 	private JTextField ip_send_field;
 	private JButton sendButton;
 	private String ipAddress;
+	private String input;
 
-	public ipFrame()
-	{		
-		setSize(400,100);
+	public ipFrame(JFrame mainFrame, String title)
+	{	
+		super(mainFrame, title, true);
+		setSize(500,100);
 		setLayout(new BorderLayout());
-		
+
 		InetAddress ip1 = null;
 		try{
 			ip1 = InetAddress.getLocalHost();
-			System.out.println("currentIP:"+ip1);
 		}catch(Exception e){
 
 		}
 
 		ipAddress = ip1.toString();
-		JLabel prompt = new JLabel (" Please copy the IP Address Seen Here : " + ipAddress);
+		JLabel prompt = new JLabel (" Please send the IP Address Seen Here : " + ipAddress);
 		add(prompt, BorderLayout.NORTH);
-	
+
 		ip_send_field = new JTextField(20);
 		add(ip_send_field, BorderLayout.CENTER);
 
-		sendButton = new JButton ("Send");
+		sendButton = new JButton ("SEND");
 		sendButton.addActionListener(new ActionListener()
 		{
 			public void actionPerformed(ActionEvent e)
 			{
-				ipAddress = ip_send_field.getText();
-				setVisible(false);
+				try
+				{
+					ip_send_field.toString();
+					input = ip_send_field.getText();
+					setVisible(false);
+				}
+				catch (NumberFormatException nfe)
+				{
+					ip_send_field.setText("");
+					setVisible(true);
+				}
 			}
 		});
-		add(sendButton, BorderLayout.SOUTH);
 
-        this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-		
+		add(sendButton, BorderLayout.SOUTH);
 		setVisible(true);
+		this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 	}
-	
+
 	public String get_ip()
 	{
 		return ipAddress;
+	}
+
+	public String get_input()
+	{
+		return input;
+	}
+
+	public String printer(String to_print)
+	{
+		return to_print;
 	}
 }
 
